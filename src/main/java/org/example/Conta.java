90e402386649e6374c65fa23a685488e2c995b14
narakushi <taina.santana1305@gmail.com>
2025-03-18 17:35:24 -0300

    teste

--- src/main/java/org/example/Conta.java
+++ src/main/java/org/example/Conta.java
@@ -3,7 +3,6 @@ package org.example;
 public abstract class Conta {
     private Double saldo;
 
-
     // criação de métodos acessores pois o atributo saldo é privado
 
     public Double getSaldo() {
